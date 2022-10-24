554e46bd953a4cd02f3fe3f649099510f5792722
oliverlowcy <104296081+oliverlowcy@users.noreply.github.com>
2022-10-24 10:23:05 +0800

    Add files via upload

---
+++ MovieScreening.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MovieScreening {
+    //private Movie movieObj;
+    //private Cinema movieScreeningLocation;
+    private Date time;
+    private ArrayList<MovieTicket> ticketsSold;
+
+    public MovieScreening(){
+
+    }
+
+    public MovieTicket createMovieTicket(){
+        MovieTicket movTix = new MovieTicket();
+        ticketsSold.add(movTix);
+        return movTix;
+    }
+
+
+//    public Cinema getCinema(){
+//        return movieScreeningLocation;
+//    }
+
+}
