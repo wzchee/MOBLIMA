d1c3d5abacda375ac9e25d3a170df94c34c8e181
Jia Rong <jiarong1202@gmail.com>
2022-10-26 23:36:49 +0800

    updated movie.class

--- Movie.java
+++ Movie.java
@@ -1,17 +1,23 @@
 import java.util.*;
-enum movieTypes{
-    ThreeD,Blockbuster, NonBlockbuster
+
+enum status{
+   Coming_Soon,Preview,Now_Showing,End_Of_Showing
   }
+enum dimension{
+  TwoD,ThreeD
+}
 public class Movie {
   private String movieTitle;
-  private String movieStatus;
   private String movieSypnosis;
   private String director;
   private String[] cast;
   private int rating;
   private String[] pastReviews;
-  private movieTypes movieType;
-  public Movie(String movieTitle, movieTypes movieType, String movieStatus, String movieSypnosis,String director,String[] cast,int rating,String[] pastReviews)
+  private dimension dims;
+  private status movieStatus;
+  private boolean blockbuster;
+  private int saleVolume;
+  public Movie(String movieTitle, int saleVolume,dimension dims, status movieStatus,boolean blockbuster, String movieSypnosis,String director,String[] cast,int rating,String[] pastReviews)
 {
   this.movieTitle = movieTitle;
   this.movieStatus = movieStatus;
@@ -20,7 +26,10 @@ public class Movie {
   this.cast = cast;
   this.rating = rating;
   this.pastReviews = pastReviews;
-  this.movieType = movieType;
+  this.dims = dims;
+  this.movieStatus = movieStatus;
+  this.blockbuster = blockbuster;
+  this.saleVolume = saleVolume;
 }
 public Movie(String movieTitle){
   this.movieTitle = movieTitle;
@@ -32,10 +41,10 @@ public String getMovieTitle(){
   return this.movieTitle;
 }
 public String getMovieStatus(){
-  return this.movieStatus;
+  return this.movieStatus.toString();
 }
-public String getMovieType(){
-  return this.movieType.toString();
+public boolean getBlockbuster(){
+  return this.blockbuster;
 }
 public String getMovieSypnosis(){
   return this.movieSypnosis;
@@ -52,13 +61,19 @@ public int getMovieRating(){
 public String[] getPastReviews(){
   return this.pastReviews;
 }
-
+public int getSaleVolume(){
+  return this.saleVolume;
+}
+public void setBlockbuster(boolean blockbuster){
+  this.blockbuster = blockbuster;
+}
 
 public void setMovieTitle(String movieTitle){
   this.movieTitle = movieTitle ;
 }
-public void setMovieStatus(String movieStatus){
+public void setMovieStatus(status movieStatus){
   this.movieStatus = movieStatus;
+
 }
 public void setMovieSypnosis(String movieSypnosis){
   this.movieSypnosis = movieSypnosis;
@@ -75,9 +90,11 @@ public void  setMovieRating(int rating){
 public void setPastReviews(String[] pastReviews){
   this.pastReviews = pastReviews;
 }
-public void setMovieType(movieTypes movietype){
-  this.movieType = movietype;
+public void editMovie(){
+
 }
+
+
 
 
 }
--- Movieapp.java
+++
@@ -1,9 +0,0 @@
-public class Movieapp{
-  public static void main(String[] args) {
-  Movie m1 = new Movie();
-  m1.setMovieType(movieTypes.Blockbuster);
-  System.out.println(m1.getMovieType());
-  System.out.println("Test");
-}
-
-}
