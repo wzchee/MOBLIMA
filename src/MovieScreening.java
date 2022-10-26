66da255e3ca3e1dcadca14178820162e3f2e4939
Oliver <oliverlowchunyi9@gmail.com>
2022-10-26 18:48:17 +0800

    First Commit

---
+++ src/MovieScreening.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MovieScreening {
+    //private Movie movieObj;
+    //private Cinema movieScreeningLocation;
+    private Date time;
+    private ArrayList<MovieTicket> ticketsSold;
+
+    public MovieScreening(){
+        System.out.println("TO TEST GIT");
+    }
+
+    public MovieTicket createMovieTicket(){
+        MovieTicket movTix = new MovieTicket();
+        ticketsSold.add(movTix);
+        return movTix;
+    }
+
+
+//    public Cinema getCinema(){
+//        return movieScreeningLocation;
+//    }
+
+}
---
+++ src/MovieTicket.java
@@ -0,0 +1,22 @@
+public class MovieTicket {
+    private MovieScreening aMovieScreening;
+    //private Seat mySeat;
+    //private User userid;
+
+
+//    public Cinema getLocation(){
+//        return aMovieScreening.getCinema();
+//    }
+    public MovieTicket(){
+
+    }
+//    public Seat getSeat(){
+//        return this.mySeat;
+//    }
+//
+//    public User getUser(){
+//        return this.userid;
+//    }
+
+
+}
