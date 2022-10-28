a774d8e9b17435d9dcaab1b5740e75db12dc2807
Chee Wen Zhan <wzchee88@gmail.com>
2022-10-29 01:00:31 +0800

    Formatting changes
    - Removed unnecessary comments

--- MOBLIMA.java
+++ MOBLIMA.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.moblima;
 import java.util.Scanner;
-/**
- *
- * @author Chee Wen Zhan
- */
+
 public class MOBLIMA {
 
     public static void main(String[] args) {
