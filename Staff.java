b9b5c45fafed4944123c6297b0be3781abd3d15c
Chee Wen Zhan <wzchee88@gmail.com>
2022-10-29 01:18:00 +0800

    Added attributes to User.java and Staff.java
    - Added main(), constructor, email, password and workplace attributes to
      Staff.java
    - Added main(), constructor, email, password, age, name and mobileNumber
      attributes to User.java

--- Staff.java
+++ Staff.java
@@ -1,3 +1,23 @@
 public class Staff {
     
+    public static void main(String[] args){
+        // User interface after logged into MOBLIMA as staff
+
+    }
+
+    public Staff(String email, String password, Cineplex workplace){
+        this.email = email;
+        this.password = password;
+        this.workplace = workplace;
+    }
+
+    private String email;
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email = email;}
+
+    private String password; //don't think a get and set applies here
+
+    private Cineplex workplace;
+    public Cineplex getWorkplace(){return workplace;}
+    public void setWorkplace(Cineplex workplace){this.workplace = workplace;}
 }
--- User.java
+++ User.java
@@ -1,3 +1,34 @@
 public class User {
     
+    public static void main(String[] args){
+        // User interface after logged into MOBLIMA as user
+
+    }
+
+    public User(String email, String password, int age, String name, String mobileNumber){
+        // constructor
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+    }
+
+    private String email;
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email = email;}
+
+    private String password; //don't think a get and set applies here
+
+    private int age;
+    public int getAge(){return age;}
+    public void setAge(int age){this.age = age;}
+
+    private String name;
+    public String getName(){return name;}
+    public void setName(int name){this.name = name;}
+
+    private String mobileNumber;
+    public String getMobileNumber(){return mobileNumber;}
+    public void setMobileNumber(String mobileNumber){this.mobileNumber = mobileNumber;}
 }
