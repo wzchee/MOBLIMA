566c17e4d8dfb7ee7b9a245299fe33ee73377808
Chee Wen Zhan <wzchee88@gmail.com>
2022-10-30 13:52:44 +0800

    Initialize File Reading using CharBuffer
    - Initialize staffin as FileReader object reading from staff.txt
    - Initialize stafftxt as CharBuffer to contain text from staff.txt
    - Initialize inputbuf as CharBuffer to contain text from user input
    - Initialize stafflist as ArrayList<String> to store staff data (may not
      be needed)
    - Read from staffin into stafftxt

--- MOBLIMA.java
+++ MOBLIMA.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.io.*;
+import java.nio.CharBuffer;
 
 public class MOBLIMA {
 
@@ -21,12 +25,26 @@ public class MOBLIMA {
                 email = input.next();
                 System.out.print("Enter your password: ");
                 password = input.next();
-                
+
+                // the following code will use the CharBuffer object, which simplifies comparison between String objects
+                InputStreamReader staffin = new FileReader("staff.txt"); //allows reading from the file staff.txt
+                CharBuffer stafftxt = CharBuffer.allocate(1000); //CharBuffer for reading from staff.txt
+                CharBuffer inputbuf = CharBuffer.allocate(1000); //User input converted into CharBuffer
+                ArrayList<String> stafflist = new ArrayList<String>(); //to store staff data
+                staffin.read(stafftxt);
                 if(email.equals("admin") && password.equals("admin")){
                     Staff.main(["admin"]);
                 }
                 break;
             case 2:
+                System.out.println("\nWelcome USER!");
+                System.out.print("Enter your email address: ");
+                email = input.next();
+                System.out.print("Enter your password: ");
+                password = input.next();
+                
+                InputStreamReader userin = new FileReader("user.txt");
+                ArrayList<String> userlist = new ArrayList<String>(); //to store user data
                 break;
             case 3:
                 
