054b231a90fb1af2d878ab47aff79427098f3b6e
Chee Wen Zhan <wzchee88@gmail.com>
2022-10-30 22:31:51 +0800

    Removed comments

--- MOBLIMA.java
+++ MOBLIMA.java
@@ -160,12 +160,11 @@ public class MOBLIMA {
     private static BufferMatchReturn charBufferMatch(CharBuffer txtbuffer, CharBuffer inputbuf){
         /*
          * Methodology:
-         * 1. Read the password of the current entry
-         * 2. Compare against password inputted by user
-         * 3. If passed, return match = true
-         * 4. If failed, return match = false
-         * 5. In both cases, also return txtbuffer to record the current position of the buffer
-         * 6. BufferMatchReturn object will be used to contain the multiple return values
+         * 1. Compare buffer from .txt file against user input
+         * 2. If passed, return match = true
+         * 3. If failed, return match = false
+         * 4. In both cases, also return txtbuffer to record the current position of the buffer
+         * 5. BufferMatchReturn object will be used to contain the multiple return values
          * 
          * Apparently compareTo() only compares the buffer from the CURRENT position
          * So there is a need to reset the position to zero before comparing
