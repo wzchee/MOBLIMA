228f038b897aeea19856c0ac9a39e1f2337dac9b
Chee Wen Zhan <wzchee88@gmail.com>
2022-11-11 22:31:56 +0800

    Shifted files into folders models and database

--- database/FileInOut.java
+++ database/FileInOut.java
@@ -1,3 +1,4 @@
+package database;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
--- movieTypes.java
+++
@@ -1,3 +0,0 @@
-// public enum movieTypes{
-//     ThreeD,Blockbuster, NonBlockbuster
-//   }
